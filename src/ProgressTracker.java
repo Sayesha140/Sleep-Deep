8df76a3c0be8fe9fce5356b9dcf351cf52e368e7
Sayesha140 <enaya871@gmail.com>
2025-03-27 06:08:03 +0600

    goal progress tracking feature completed.

---
+++ src/ProgressTracker.java
@@ -0,0 +1,61 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProgressTracker implements IReportGenerator{
+
+    IGoalManager goalManager;
+    ISleepLogManager logManager;
+
+    public ProgressTracker(IGoalManager goalManager,ISleepLogManager logManager){
+        this.goalManager=goalManager;
+        this.logManager=logManager;
+    }
+
+    @Override
+
+    public List<Map<String, String>> generateReport() {
+
+        List<Map<String, String>> reportData = new ArrayList<>();
+        List<Goal> goals = goalManager.getGoals();
+
+        for (Goal goal : goals) {
+            LocalDate date = goal.getDate();
+            double targetHours = goal.getTargetHours();
+            double totalSleptHours = logManager.calculateTotalSleepDurationByDate(date);
+            String verdict = getVerdict(totalSleptHours, targetHours);
+
+            Map<String, String> entry = new HashMap<>();
+            entry.put("Date", date.toString());
+            entry.put("Target Hours", String.format("%.2f", targetHours));
+            entry.put("Hours Slept", String.format("%.2f", totalSleptHours));
+            entry.put("Status", verdict);
+
+            reportData.add(entry);
+        }
+
+        return reportData;
+    }
+
+    private String getVerdict(double totalSleptHours,double targetHours){
+
+        String verdict;
+
+        if (totalSleptHours==0){
+            verdict="-------";
+        }
+        else if (totalSleptHours>targetHours+2) {
+            verdict="Overslept";
+        }
+        else if(totalSleptHours>=targetHours){
+            verdict="Goal Met";
+        }
+        else {
+            verdict="Missed Goal";
+        }
+        return verdict;
+    }
+
+}
