9fdcafe53c40024b6e4157ec25f35e4e459b627d
Sayesha140 <enaya871@gmail.com>
2025-04-05 19:02:18 +0600

    updated

--- src/DaytimeFatigueTips.java
+++ src/DaytimeFatigueTips.java
@@ -2,8 +2,8 @@ public class DaytimeFatigueTips implements ITipGenerator {
     @Override
     public String generateTips() {
         String[] tips = {
-                "1.Ensure you’re getting at least 7 to 9 hours of quality sleep.",
-                "2.Try short power naps (20 to30 mins) if you're tired.",
+                "1.Ensure you're getting at least 7 to 9 hours of quality sleep.",
+                "2.Try short power naps (20 to3 0 mins) if you're tired.",
                 "3.Stay hydrated and eat balanced meals.",
                 "4.Take breaks from screens and stretch often."
         };
