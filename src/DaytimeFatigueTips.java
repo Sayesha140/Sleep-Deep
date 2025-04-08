f80929f4fe8bec8a7b701586903f24eeeaaf00a0
Sayesha140 <enaya871@gmail.com>
2025-04-08 11:37:40 +0600

    updated

--- src/DaytimeFatigueTips.java
+++ src/DaytimeFatigueTips.java
@@ -3,7 +3,7 @@ public class DaytimeFatigueTips implements ITipGenerator {
     public String generateTips() {
         String[] tips = {
                 "1.Ensure you're getting at least 7 to 9 hours of quality sleep.",
-                "2.Try short power naps (20 to3 0 mins) if you're tired.",
+                "2.Try short power naps (20 to 30 mins) if you're tired.",
                 "3.Stay hydrated and eat balanced meals.",
                 "4.Take breaks from screens and stretch often."
         };
--- src/QuickTipGenerator.java
+++ src/QuickTipGenerator.java
@@ -18,7 +18,7 @@ public class QuickTipGenerator implements ITipGenerator{
             "Soft lighting, a comfy mattress, and a clean room can turn your bedroom into the ultimate sleep sanctuary!",
             "Afternoon naps are great, but avoid napping too late. It can affect your ability to fall asleep at night.",
             "Your bed should be a haven for relaxation! Make it extra cozy tonight with fresh sheets and a comfy pillow.",
-            "If you’re oversleeping in the mornings, it might be because you're staying up too late. A consistent bedtime will make your mornings easier and more energetic!",
+            "If you're oversleeping in the mornings, it might be because you're staying up too late. A consistent bedtime will make your mornings easier and more energetic!",
             "If you did not get enough sleep last night, a power nap (10-20 minutes) during the day can refresh you without disrupting your nighttime sleep!",
             "Oversleeping may sound like a luxury, but it actually messes with your sleep cycle and can make you feel sluggish. Time to rise and shine!",
             "Aim for 7-9 hours of sleep. Too little leaves you sluggish, too much leaves you groggy! Find your sweet spot for the perfect amount of sleep."
